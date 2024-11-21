afcf08334ede7f9c4dfcf0b51d63ee5f12caaca2
Noel Murasko <murasko@ualberta.ca>
2024-11-21 16:38:44 -0700

    Update python script.

--- tests.py
+++ tests.py
@@ -1,38 +1,45 @@
+#!/usr/bin/env python
+
 import os
 import subprocess
 
-directory=f".{os.sep}examples"
-command = "asy"
+def main():
+  directory=f".{os.sep}examples"
+  command = "asy"
 
-passed="PASSED"
-failed="FAILED"
+  passed="PASSED"
+  failed="FAILED"
 
-verbose=True
-#print("\033[<style>;<text_color>;<background_color>mYour Text\033[0m")
-failureCount=0
-failedTests=[]
-asyfiles=[file for file in sorted(os.listdir(directory)) if file.endswith(".asy")]
-totaltests=len(asyfiles)
-print(f"Testing {totaltests} files\n")
+  verbose=True
+  #print("\033[<style>;<text_color>;<background_color>mYour Text\033[0m")
+  failureCount=0
+  failedTests=[]
+  asyfiles=[file for file in sorted(os.listdir(directory)) if file.endswith(".asy")]
+  totaltests=len(asyfiles)
+  print(f"Testing {totaltests} files\n")
 
-# Iterate through files in the directory
-for j,filename in enumerate(asyfiles):
-    if filename.endswith(".asy"):
-        filepath = os.path.join(directory, filename)
-        # Run the command on the file
-        try:
-            subprocess.run([command, filepath], stdout=subprocess.DEVNULL,  stderr=subprocess.STDOUT,check=True)
-            if verbose:
-              print(f"{f"{j+1}. {filename}":<30} {passed:>10}")
-        except subprocess.CalledProcessError as e:
-            print(f"{f"{j+1}. {filename}":<30} {failed:>10}")
-            failureCount+=1
-            failedTests.append(filename)
+  # Iterate through files in the directory
+  for j,filename in enumerate(asyfiles):
+      if filename.endswith(".asy"):
+          filepath = os.path.join(directory, filename)
+          # Run the command on the file
+          try:
+              subprocess.run([command, filepath], stdout=subprocess.DEVNULL,  stderr=subprocess.STDOUT,check=True)
+              if verbose:
+                print(f"{f"{j+1}. {filename}":<30} {passed:>10}")
+          except subprocess.CalledProcessError as e:
+              print(f"{f"{j+1}. {filename}":<30} {failed:>10}")
+              failureCount+=1
+              failedTests.append(filename)
 
 
-if len(failedTests) > 0:
-  print(f"\n{failureCount} out of {totaltests} tests failed:")
-  for test in failedTests:
-    print(test)
-else:
-  print("\nAll tests passed!")
+  if len(failedTests) > 0:
+    print(f"\n{failureCount} out of {totaltests} tests failed:")
+    for test in failedTests:
+      print(test)
+  else:
+    print("\nAll tests passed!")
+
+
+if __name__ == "__main__":
+  main()
